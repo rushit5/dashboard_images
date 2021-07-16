bd3df79feb8b95f93cec24529f14dbd81f95e357
rushit5 <77121716+rushit5@users.noreply.github.com>
2021-07-16 14:48:12 -0400

    Update am_charts_funnel.js

--- am_charts_funnel.js
+++ am_charts_funnel.js
@@ -77,8 +77,8 @@ for(var row of data) {
 //}];
 	
 // Themes begin
+am4core.useTheme(am4themes_material);	
 am4core.useTheme(am4themes_animated);
-am4core.disposeAllCharts();	
 // Themes end
 	
 var chart = am4core.create("container", am4charts.SlicedChart);
